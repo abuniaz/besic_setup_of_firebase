63cf92fac9b67da889ef87b05d5229079c15935c
Abu Niaz <niaz35-507@diu.edu.bd>
2022-11-16 23:22:07 +0600

    yes

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,14 @@
-import 'package:basic_firebase/Login_Page/login_screen.dart';
-import 'package:basic_firebase/home.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const MaterialApp(
-              home: Scaffold(
-                body: Center(
-                  child: Text(
-                    "This App is being initialized",
-                    style: TextStyle(
-                        color: Colors.cyan,
-                        fontSize: 48,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Signatra'),
-                  ),
-                ),
-              ),
-            );
-          } else if (snapshot.hasError) {
-            return const MaterialApp(
-              home: Scaffold(
-                body: Center(
-                  child: Text(
-                    "An error has been occurred",
-                    style: TextStyle(
-                        color: Colors.cyan,
-                        fontSize: 48,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            );
-          }
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Basic Firebase Connect',
-            theme: ThemeData(
-                scaffoldBackgroundColor: Colors.blueGrey,
-                primarySwatch: Colors.blue),
-            home: const LoginScreen(),
-          );
-        });
+    return Container();
   }
 }
