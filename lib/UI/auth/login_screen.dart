a2a9945076ec6ba1622e2f8bdae99d8a2b02734a
Abu Niaz <niaz35-507@diu.edu.bd>
2022-11-19 14:39:06 +0600

    Update login_screen.dart

--- lib/UI/auth/login_screen.dart
+++ lib/UI/auth/login_screen.dart
@@ -9,6 +9,18 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwoardController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    emailController.dispose();
+    passwoardController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,14 +29,45 @@ class _LoginScreenState extends State<LoginScreen> {
         title: const Text('Login'),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          RoundButton(
-            title: 'Login',
-            onTap: () {},
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                        suffixIcon: Icon(Icons.email),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      controller: passwoardController,
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                        suffixIcon: Icon(Icons.visibility_outlined),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                )),
+            RoundButton(
+              title: 'Login',
+              onTap: () {},
+            )
+          ],
+        ),
       ),
     );
   }
